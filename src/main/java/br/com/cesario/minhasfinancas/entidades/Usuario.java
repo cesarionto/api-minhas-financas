4546fd4bb805e49d880fc293708274e09766e530
cesarionto <cesariopereiraneto@gmail.com>
2020-07-18 12:23:40 -0300

    Mapeando a entidade Usuario

---
+++ src/main/java/br/com/cesario/minhasfinancas/entidades/Usuario.java
@@ -0,0 +1,82 @@
+package br.com.cesario.minhasfinancas.entidades;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name = "usuario", schema = "financas")
+public class Usuario {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "nome")
+    private String nome;
+
+    @Column(name = "email")
+    private String email;
+
+
+    @Column(name = "senha")
+    private String senha;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Usuario usuario = (Usuario) o;
+        return id == usuario.id &&
+                nome.equals(usuario.nome) &&
+                email.equals(usuario.email) &&
+                senha.equals(usuario.senha);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, nome, email, senha);
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", email='" + email + '\'' +
+                ", senha='" + senha + '\'' +
+                '}';
+    }
+}
