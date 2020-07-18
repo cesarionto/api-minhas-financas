f539c2d586ccf98614a82ed22cc585cbc9b669ef
cesarionto <cesariopereiraneto@gmail.com>
2020-07-18 12:14:09 -0300

    configuração do banco de dados postgres

---
+++ src/test/java/br/com/cesario/minhasfinancas/MinhasfinancasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cesario.minhasfinancas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinhasfinancasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
