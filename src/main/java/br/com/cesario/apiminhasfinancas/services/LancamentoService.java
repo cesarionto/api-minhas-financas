8c9337c0a43f347a60a4ea651a2894af67f67c0a
cesarionto <cesariopereiraneto@gmail.com>
2020-07-24 10:53:24 -0300

    Implementando os metudos de servico de lancamento.

---
+++ src/main/java/br/com/cesario/apiminhasfinancas/services/LancamentoService.java
@@ -0,0 +1,20 @@
+package br.com.cesario.apiminhasfinancas.services;
+
+import br.com.cesario.apiminhasfinancas.enums.StatusLancamento;
+import br.com.cesario.apiminhasfinancas.models.entidades.Lancamento;
+
+import java.util.List;
+
+public interface LancamentoService {
+
+    Lancamento salvar(Lancamento lancamento);
+
+    Lancamento atualizar(Lancamento lancamento);
+
+    void deletar(Lancamento lancamento);
+
+    List<Lancamento> buscar (Lancamento lancamentoFiltro);
+
+    void atualizarStatus(Lancamento lancamento, StatusLancamento statusLancamento);
+
+}
---
+++ src/main/java/br/com/cesario/apiminhasfinancas/services/implementations/LancamentoServiceImplementation.java
@@ -0,0 +1,59 @@
+package br.com.cesario.apiminhasfinancas.services.implementations;
+
+import br.com.cesario.apiminhasfinancas.enums.StatusLancamento;
+import br.com.cesario.apiminhasfinancas.models.JpaRepository.LancamentoRepository;
+import br.com.cesario.apiminhasfinancas.models.entidades.Lancamento;
+import br.com.cesario.apiminhasfinancas.services.LancamentoService;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Objects;
+
+@Service
+public class LancamentoServiceImplementation implements LancamentoService {
+
+    private LancamentoRepository lancamentoRepository;
+
+    public LancamentoServiceImplementation(LancamentoRepository lancamentoRepository){
+        this.lancamentoRepository = lancamentoRepository;
+    }
+
+    @Override
+    @Transactional
+    public Lancamento salvar(Lancamento lancamento) {
+        return lancamentoRepository.save(lancamento);
+    }
+
+    @Override
+    @Transactional
+    public Lancamento atualizar(Lancamento lancamento) {
+        Objects.requireNonNull(lancamento.getId());
+        return lancamentoRepository.save(lancamento);
+    }
+
+    @Override
+    public void deletar(Lancamento lancamento) {
+        Objects.requireNonNull(lancamento.getId());
+        lancamentoRepository.delete(lancamento);
+    }
+
+    @Override
+    @Transactional
+    public List<Lancamento> buscar(Lancamento lancamentoFiltro) {
+        Example example = Example.of(lancamentoFiltro,
+                ExampleMatcher.matching()
+                .withIgnoreCase()
+                .withStringMatcher(ExampleMatcher.StringMatcher.CONTAINING));
+        return lancamentoRepository.findAll(example);
+    }
+
+    @Override
+    public void atualizarStatus(Lancamento lancamento, StatusLancamento statusLancamento) {
+        lancamento.setStatus(statusLancamento);
+        atualizar(lancamento);
+
+    }
+}
