526a9c7761d9f435633c452c4678798a3993862d
cesarionto <cesariopereiraneto@gmail.com>
2020-08-30 14:28:20 -0300

    adicionando o CORS

--- src/main/java/br/com/cesario/apiminhasfinancas/ApiMinhasfinancasApplication.java
+++ src/main/java/br/com/cesario/apiminhasfinancas/ApiMinhasfinancasApplication.java
@@ -2,9 +2,20 @@ package br.com.cesario.apiminhasfinancas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @SpringBootApplication
-public class ApiMinhasfinancasApplication {
+@EnableWebMvc
+public class ApiMinhasfinancasApplication implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ApiMinhasfinancasApplication.class, args);
